1b277898d61ca9078800b427d774bdd2640d33c3
Etienne Warneck <etienne.warneck@gmail.com>
2019-12-10 17:25:06 -0500

    filtering paths

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,16 +1,21 @@
+
+
+
+
+
 // var path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 //Display route to the survey page
-router.GET('/survey.html', (req, res, next) => {
+router.get('/survey.html', (req, res, next) => {
     console.log('This always runs');
     res.sendFile(path.join(__dirname, '../public/home.html'));
 })
 
 //Display a default catch-all route to home.html page
-router.GET('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, '../public/survey.html'));
 
 })
--- server.js
+++ server.js
@@ -4,22 +4,24 @@ var express = require("express");
 // var path = require("path");
 var bodyparser = require('body-parser');
 
-var PORT = process.env.PORT || 3000;
-
-
 // Sets up the Express App
 // =============================================================
 var app = express();// app is a requesthandler
 
+var PORT = process.env.PORT || 3000;
 //importing 
-const apiRoutes = require('./app/routing/apiRoutes');
 const htmlRoutes = require('./app/routing/htmlRoutes'); 
+const apiRoutes = require('./app/routing/apiRoutes');
 
 //A built-in middleware function. It parses incoming requests
 app.use(bodyparser.urlencoded({ extended: false })); 
 
-app.use(htmlRoutes);
-app.use(apiRoutes);
+app.use('/htmlRoutes', htmlRoutes); //add path as a filter
+app.use('/apiRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>404 Page not found</h1>');
+});
 
 //create server and listen to it on PORT
 app.listen(PORT, function () {
