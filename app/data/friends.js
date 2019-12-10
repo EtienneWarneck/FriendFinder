f764e0bf4dc600f04e2de5c7f586a3479d5e897e
Etienne Warneck <etienne.warneck@gmail.com>
2019-12-10 10:05:39 -0500

    First commit

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var friendsArray = [
+    {
+        "name": "Woodie",
+        "photo": "",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ],
+    },
+    {
+        "name": "Shanice",
+        "photo": "",
+        "scores": [
+            2,
+            5,
+            1,
+            4,
+            3,
+            2,
+            1,
+            5,
+            5,
+            3
+        ],
+    },
+    {
+        "name": "Luna",
+        "photo": "",
+        "scores": [
+            2,
+            1,
+            5,
+            3,
+            2,
+            2,
+            3,
+            3,
+            1,
+            1
+        ],
+    },
+    {
+        "name": "Angel",
+        "photo": "",
+        "scores": [
+            1,
+            5,
+            2,
+            4,
+            4,
+            5,
+            1,
+            5,
+            4,
+            3
+        ],
+    },
+]
+
+module.exports = friendsArray;
